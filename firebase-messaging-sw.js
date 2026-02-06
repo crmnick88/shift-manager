be07c5482c9d794f64909e15d8d6873f91f4d5d1
crmnick88 <crmnick88@gmail.com>
2026-02-06 22:59:46 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyD-uaQ2aO87kGxbcc-m8_9fzJ3uhPv2zvk",
@@ -8,16 +8,14 @@ firebase.initializeApp({
   projectId: "shift-manager-c026e",
   storageBucket: "shift-manager-c026e.firebasestorage.app",
   messagingSenderId: "1091605398341",
-  appId: "1:1091605398341:web:cb6c14e7d832c3e8df342c",
+  appId: "1:1091605398341:web:cb6c14e7d832c3e8df342c"
 });
 
-// ⭐ לא מציגים notification ידנית
-firebase.messaging();
+const messaging = firebase.messaging();
 
-// רק טיפול בלחיצה
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow("https://crmnick88.github.io/")
-  );
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: './icon-192.png'
+  });
 });
