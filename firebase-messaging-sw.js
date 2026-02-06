8c2630fed2296c9bdb7d76a5b1f6f15fe776a8db
crmnick88 <crmnick88@gmail.com>
2026-02-06 23:59:14 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,18 +1,17 @@
-importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD-uaQ2aO87kGxbcc-m8_9fzJ3uhPv2zvk",
+  apiKey: "AIzaSy...",
   authDomain: "shift-manager-c026e.firebaseapp.com",
   databaseURL: "https://shift-manager-c026e-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "shift-manager-c026e",
   storageBucket: "shift-manager-c026e.firebasestorage.app",
   messagingSenderId: "1091605398341",
-  appId: "1:1091605398341:web:cb6c14e7d832c3e8df342c",
+  appId: "1:1091605398341:web:cb6c14e7d832c3e8df342c"
 });
 
 firebase.messaging();
 
-// Option A:
-// לא משתמשים כאן ב-onBackgroundMessage/showNotification.
-// ה-FCM יציג את ההתראה אוטומטית לפי webpush.notification שנשלח מהשרת.
+// ❗️ אין onBackgroundMessage ואין showNotification
+// Option A – FCM מטפל לבד
